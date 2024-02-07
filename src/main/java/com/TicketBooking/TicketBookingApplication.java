2949f1be32b30218de1b3264d070e3eaf912abc3
stephy-VS <vsstephy.ptf@gmail.com>
2024-02-07 01:36:48 -0500

    InstitutionRegistrationDetails

--- src/main/java/com/TicketBooking/TicketBookingApplication.java
+++ src/main/java/com/TicketBooking/TicketBookingApplication.java
@@ -2,8 +2,12 @@ package com.TicketBooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "com.TicketBooking.InstitutionRegistration")
+//@ComponentScan(basePackages = "com.TicketBooking")
 public class TicketBookingApplication {
 
 	public static void main(String[] args) {
